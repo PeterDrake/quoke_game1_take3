5cf90aaecbc20c15b834252c8e891464634f6b5e
Peter Drake <drake@lclark.edu>
2021-10-25 09:07:41 -0700

    Fixed missing references to objects in MoveToggles script attached to Canvi in L2.

--- Assets/Scripts/ChangeMusicSoundValues.cs
+++ Assets/Scripts/ChangeMusicSoundValues.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ChangeMusicSoundValues : MonoBehaviour
 {
-    private GameObject mHandle;
-    private GameObject sHandle;
+    // private GameObject mHandle;
+    // private GameObject sHandle;
 
 
     void Start()
